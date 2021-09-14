c00ac7b3c0a35ceb5c04c11683862d0794617138
zhangyifan <zhangyifan@rongcloud.cn>
2021-09-14 16:29:02 +0800

    Initial Commit

---
+++ CategroyTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CategroyTest
+//
+//  Created by zhangyifan on 2021/9/14.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
